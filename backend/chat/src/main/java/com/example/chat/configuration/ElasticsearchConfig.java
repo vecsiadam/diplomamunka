d4251647eae56d2c17c5be490fca0ae1d48454b9
adamvecsi <vecsi1994@hotmail.com>
2020-10-19 22:54:08 +0200

    feat: elasticsearch config

---
+++ backend/chat/src/main/java/com/example/chat/configuration/ElasticsearchConfig.java
@@ -0,0 +1,40 @@
+package com.example.chat.configuration;
+
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.RestClients;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+
+/**
+ * TODO: ElasticsearchConfig
+ *
+ * @author Adam Vecsi
+ * @since 2020. 10. 19
+ */
+
+@Configuration
+@EnableElasticsearchRepositories(basePackages = "com.example.chat.repository")
+// @ComponentScan(basePackages = { "com.example.chat..service" })
+public class ElasticsearchConfig {
+
+	@Value("${chat.elasticsearch.url:localhost:9200}")
+	private String elasticsearchUrl;
+
+	@Bean
+	public RestHighLevelClient client() {
+		ClientConfiguration clientConfiguration = ClientConfiguration.builder().connectedTo(this.elasticsearchUrl)
+				.build();
+
+		return RestClients.create(clientConfiguration).rest();
+	}
+
+	@Bean
+	public ElasticsearchOperations elasticsearchTemplate() {
+		return new ElasticsearchRestTemplate(this.client());
+	}
+}
