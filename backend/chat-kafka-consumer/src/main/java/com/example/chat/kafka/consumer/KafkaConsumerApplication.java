2a93796256efa6cfa12c17ef54691fa4ee24c4e2
adamvecsi <vecsi1994@hotmail.com>
2020-10-28 18:13:00 +0100

    fix: kafka consumer messageService bean

--- backend/chat-kafka-consumer/src/main/java/com/example/chat/kafka/consumer/KafkaConsumerApplication.java
+++ backend/chat-kafka-consumer/src/main/java/com/example/chat/kafka/consumer/KafkaConsumerApplication.java
@@ -2,8 +2,8 @@ package com.example.chat.kafka.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpEntity;
-import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import com.example.chat.api.service.MessageSaveService;
@@ -15,16 +15,22 @@ public class KafkaConsumerApplication {
 		SpringApplication.run(KafkaConsumerApplication.class, args);
 	}
 
-	@Service
-	public class MessageSaveServiceImpl implements MessageSaveService {
+	/**
+	 * If run standalone Kafka Consumer application
+	 *
+	 * @return
+	 */
+	@Bean
+	public MessageSaveService messageService() {
+		return new MessageSaveService() {
 
-		@Override
-		public void saveMessage(String value) {
-			RestTemplate restTemplate = new RestTemplate();
-			HttpEntity<String> request = new HttpEntity<>(value);
-			restTemplate.postForObject("http://localhost:8082/message/save", request, String.class);
-
-		}
+			@Override
+			public void saveMessage(String value) {
+				RestTemplate restTemplate = new RestTemplate();
+				HttpEntity<String> request = new HttpEntity<>(value);
+				restTemplate.postForObject("http://localhost:8082/message/save", request, String.class);
+			}
+		};
 
 	}
 
