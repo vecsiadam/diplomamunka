64bd2a3787359f78918c79ab1a86ae7d36d3ba0a
adamvecsi <vecsi1994@hotmail.com>
2020-10-30 21:33:48 +0100

    feat: serch in message

--- backend/chat/src/main/java/com/example/chat/repository/MessageElasticsearchRepository.java
+++ backend/chat/src/main/java/com/example/chat/repository/MessageElasticsearchRepository.java
@@ -1,8 +1,10 @@
 package com.example.chat.repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.data.elasticsearch.annotations.Query;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
 import com.example.chat.entity.ElasticsearchMessageEntity;
@@ -16,5 +18,11 @@ import com.example.chat.entity.ElasticsearchMessageEntity;
 public interface MessageElasticsearchRepository extends ElasticsearchRepository<ElasticsearchMessageEntity, UUID> {
 
 	List<ElasticsearchMessageEntity> findTop5ByRoomRoomIdOrderByDateTimeDesc(Long roomId);
+
+	List<ElasticsearchMessageEntity> findAllByDateTimeBetweenAndRoomRoomId(LocalDateTime from, LocalDateTime to,
+			Long roomId);
+
+	@Query("{\"bool\":{\"must\":{\"match\":{\"message\":\"?1\"}},\"filter\":{\"term\":{\"room.roomId\":?0}}}}")
+	List<ElasticsearchMessageEntity> findByMessageAndRoomRoomId(Long roomId, String search);
 
 }
